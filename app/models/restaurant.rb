ac2dc2922738132e09c01afa5428a2beef7e848c
Travis Benge <travis.l.benge@gmail.com>
2018-05-16 16:01:43 +0200

    Rails

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,5 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true, uniqueness: true
   validates :phone_number, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
-
   has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+puts "Creating 5 fake restaurants..."
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Book.title,
+    address: Faker::Address.city,
+    phone_number: Faker::Address.postcode,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+end
+puts "Finished!"
